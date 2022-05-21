2225f0e16c4bd509e0dcb6296090d03d4206d5c5
givensuman <givensuman@gmail.com>
2022-05-21 15:12:09 -0400

    Made in React

---
+++ react/src/main.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import '../../style.css'
+
+interface Todo {
+  text: string,
+  remove: () => void
+}
+
+const Todo = ({ text, remove }: Todo) => {
+  const [ isComplete, setIsComplete ] = useState(false)
+  return (
+    <div
+      className={`todos-container ${isComplete && 'complete'}`}
+      onClick={() => setIsComplete(state => !state)}
+    >
+      <span className="todos-todo">{text}</span>
+      <button 
+        className="todos-delete"
+        onClick={remove}
+      />
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [ input, setInput ] = useState("")
+  const [ todos, setTodos ] = useState<string[]>([])
+
+  const addTodo = () => {
+      setTodos(state => [...state, input])
+      setInput("")
+  }
+
+  const removeTodo = (index: number) => {
+    setTodos(state => state.filter((_, i) => i !== index))
+  }
+
+  return (
+    <div id="app">
+
+      <h1 className="header">React</h1>
+
+      <div className="input-wrapper">
+        <input
+          type="text"
+          placeholder="Create new to-do"
+          className="input-input"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          onKeyDown={e => { if (e.key === 'Enter') addTodo() }}
+        />
+        <button 
+          className="input-button"
+          onClick={addTodo}
+        >
+          Add
+        </button>
+      </div>
+
+      <div className="divider" />
+
+      <div className="todos-wrapper">
+        {todos.map((item, index) => 
+            <Todo
+              key={index}
+              text={item}
+              remove={() => removeTodo(index)}
+            />
+          )}
+      </div>
+
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
