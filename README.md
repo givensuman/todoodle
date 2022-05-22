9fd3652d0fd9be377e531530784b827f0d97bc54
givensuman <givensuman@gmail.com>
2022-05-22 15:23:36 -0400

    Finish react

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # todoodle
+### An attempt to make a to-do list with every JavaScript framework I can
 
-An attempt to make a to-do app in every JavaScript framework.
+## rules
+ - Each app must be made in one `.js` file (if possible)
+ - No interaction with HTML unless intended by the framework
+ - The only inline-styling each framework needs to allow me to do is for the header
+ - Each app needs to save and load state in `localStorage`
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,90 +1,115 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import '../../style.css'
 
-interface Todo {
-  text: string,
-  remove: () => void
-}
-
-const Todo = ({ text, remove }: Todo) => {
-  const [ isComplete, setIsComplete ] = useState(false)
-  return (
-    <div
-      className={`todos-container ${isComplete && 'complete'}`}
-      onClick={e => {
-        e.stopPropagation()
-        setIsComplete(state => !state)
-      }}
-    >
-      <span className="todos-todo">{text}</span>
-      <button 
-        className="todos-delete"
-        onClick={e => {
-          e.stopPropagation()
-          remove()
-        }}
-      />
-    </div>
-  )
-}
-
 const App = () => {
 
+  type Todo = {
+    value: string,
+    completed: boolean
+  }
+  const [ todos, setTodos ] = useState<Todo[]>([])
+  const [ completedCount, setCompletedCount ] = useState(0)
   const [ input, setInput ] = useState("")
-  const [ todos, setTodos ] = useState<string[]>([])
+
+  const readLocalStorage = (key: string, fallback: any) => {
+    if (localStorage.getItem(key)) return JSON.parse(localStorage[key])
+    else return fallback
+  }
+
+  const writeLocalStorage = (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  const getQuote = async () => {
+    await fetch('https://type.fit/api/quotes')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+  useEffect(() => {
+    setTodos(readLocalStorage('todoodle--todos', []))
+    setCompletedCount(readLocalStorage('todoodle--completedCount', 0))
+    getQuote()
+  }, [])
 
   const addTodo = () => {
-      setTodos(state => [...state, input])
-      setInput("")
+    if (input === "") return
+    const extendedState = [...todos, {
+      value: input, completed: false
+    }]
+    writeLocalStorage('todoodle--todos', extendedState) 
+    setTodos(extendedState)
+    setInput('')
+  }
+
+  const toggleTodo = (index: number) => {
+    const shallowState = [...todos]
+    shallowState[index].completed = !shallowState[index].completed
+    setTodos(shallowState)
+    writeLocalStorage('todoodle--todos', shallowState)
   }
 
   const removeTodo = (index: number) => {
-    setTodos(state => state.filter((_, i) => i !== index))
+    if (todos[index].completed) {
+      writeLocalStorage('todoodle--completedCount', completedCount + 1)
+      setCompletedCount(state => state + 1)
+    }
+    const filteredState = [...todos].filter((_, i) => i !== index)
+    setTodos(filteredState)
+    writeLocalStorage('todoodle--todos', filteredState)
   }
 
   return (
     <div id="app">
 
-      <h1 
-        className="header"
-        style={{
-          color: "#2196f3"
-        }}
+    <h1 
+      className="header"
+      style={{
+        color: "#2196f3",
+        textShadow: "1px 1px #82b1ff, 2px 2px #263238"
+      }}
+    >
+      React
+    </h1>
+
+    <h3 className="completed-count">{completedCount} completed tasks</h3>
+
+    <div className="input-wrapper">
+      <input 
+        className="input-input"
+        type="text"
+        placeholder="Create a new to-do"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        onKeyDown={e => e.key === "Enter" && addTodo()}
+      />
+      <button
+        className="input-button"
+        onClick={addTodo}
       >
-        React
-      </h1>
+        Add
+      </button>
+    </div>
 
-      <div className="input-wrapper">
-        <input
-          type="text"
-          placeholder="Create new to-do"
-          className="input-input"
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          onKeyDown={e => { 
-            if (e.key === 'Enter') addTodo() 
-          }}
-        />
-        <button 
-          className="input-button"
-          onClick={addTodo}
+    <div className="todos-wrapper">
+      {todos.map((task, index) => 
+        <div
+          key={index}
+          className={`todos-container ${task.completed && "completed"}`}
+          onClick={() => toggleTodo(index)}
         >
-          Add
-        </button>
-      </div>
-
-      <div className="divider" />
-
-      <div className="todos-wrapper">
-        {todos.map((item, index) => 
-            <Todo
-              key={index}
-              text={item}
-              remove={() => removeTodo(index)}
-            />
-          )}
-      </div>
+          <span className="todos-todo">{task.value}</span>
+          <button 
+            className="todos-delete"
+            onClick={e => {
+              e.stopPropagation()
+              removeTodo(index)
+            }}
+          />
+        </div>
+      )}
+    </div>
 
     </div>
   )
