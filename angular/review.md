d51145ea3766b57bc1e35c1ded64b9ed394990a2
givensuman <givensuman@gmail.com>
2022-05-25 11:31:35 -0400

    Add angular

---
+++ angular/review.md
@@ -0,0 +1,12 @@
+## Overall - 6/10
+I didn't really like Angular. I'm sure it's fantastic for huge teams, and I think I understand how as an Angular developer it's easy to jump into a foreign component and figure it out. Yes, it has an insanely powerful CLI and Google-built ecosystem. I just didn't think it was fun to write. Granted, Angular is probably not the choice anyone would make for a to-do app this simple, but on a component-by-component basis I don't think it's 
+
+In summary, I see it's value, and I'm sure it's powerful if you're used to it, I just think there's other frameworks doing things better.
+
+Also it wouldn't apply my stylesheet and I have no idea why.
+
+## Documentation/Help - 6/10
+If Vue documentation is a well-built manual that tells you what you need to know, Angular documentation is a textbook from 30 years ago that is overwhelming and foreign. Documentation is exhaustive and there are plenty of examples, it's just there's a ton of it and I didn't find beginner-friendliness to be the forefront in the writing.
+
+## API - 6/10
+I get why the API is good, it's opinionated, has discrete separation of concerns, etc. What I think it isn't is intuitive. I was pleasantly surprised at how quickly I felt I understood an API like Mithril's, once I understood the core concepts every other concept seemed to stem from them naturally. With Angular? Well, if you need input modeling, you need to import `FormsModules`, then apply it to your `.module.ts`, then bind it to your input with `[(ngModel)]` which is a binding syntax I didn't see anywhere else. Compare that to Vue, which is simply `v-model="..."`.
---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,114 @@
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core'
+
+type Todo = {
+  value: string,
+  completed: boolean
+}
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['../../../style.css'],
+  template:`
+    <div id='app'>
+
+      <h1 class='header'>Angular</h1>
+
+      <div class="completed-wrapper">
+        <h3 class="completed-count">
+          {{ completedCount }} completed tasks
+        </h3>
+        <button 
+          class="completed-reset"
+          (click)="completedCount = 0"
+        >
+          Reset
+        </button>
+      </div>
+
+      <div class="input-wrapper">
+        <input 
+          class="input-input"
+          type="text"
+          name="input-thing"
+          [(ngModel)]="input"
+        />
+        <button 
+          class="input-button"
+          (click)="addTodo()"
+        >
+          Add
+        </button>
+      </div>
+
+      <div class="todos-wrapper">
+        <div 
+          class="todos-container"
+          *ngFor="let task of todos; let index = index;"
+          [class.completed]="task.completed"
+          (click)="toggleTodo(index)"
+        >
+          <span class="todos-todo">{{ task.value }}</span>
+          <button 
+          class="todos-delete"
+          (click)="removeTodo(index)"
+          ></button>
+        </div>
+      </div>
+
+    </div>
+  `
+})
+export class AppComponent implements OnInit, DoCheck {
+  title = 'angular'
+
+  todos: Todo[] = []
+  completedCount: number = 0
+  input: string = ""
+
+
+  readLocalStorage = (key: string, fallback: any) => {
+    if (localStorage.getItem(key)) return JSON.parse(localStorage[key])
+    else return fallback
+  }
+
+  writeLocalStorage = (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  ngOnInit(): void {
+      this.todos = this.readLocalStorage('todoodle--todos', [])
+      this.completedCount = this.readLocalStorage('todoodle--completedCount', 0)
+  }
+
+  oldTodos: Todo[] = []
+  oldCompletedCount: number = 0
+
+  ngDoCheck() {
+    if (this.todos !== this.oldTodos) {
+      this.writeLocalStorage('todoodle--todos', this.todos)
+      this.oldTodos = this.todos
+    }
+    if (this.completedCount !== this.oldCompletedCount) {
+      this.writeLocalStorage('todoodle--completedCount', this.completedCount)
+      this.oldCompletedCount = this.completedCount
+    }
+  }
+
+  handleInput = (e: { currentTarget: { value: string } }) => 
+    { this.input = e.currentTarget.value }
+
+  addTodo = () => {
+    if (this.input !== "") {
+      this.todos = [...this.todos, { value: this.input, completed: false }]
+    }
+  }
+
+  removeTodo = (index: number) => {
+    if (this.todos[index].completed) this.completedCount++
+    this.todos = this.todos.filter((_, i) => i !== index)
+  }
+
+  toggleTodo = (index: number) => {
+    this.todos[index].completed = !this.todos[index].completed
+  }
+}
